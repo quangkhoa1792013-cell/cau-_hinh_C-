81cd41d0320bff0570c0ffb65aba94be83fca523
Máy số 6 <quangkhoa1792013@gmail.com>
2026-04-22 10:35:05 +0700

    Update v16.0: 22/04/2026 10:35:05,25

---
+++ tinhoctre/thi/purple02a/lienhe.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    vector<int> a;
+
+    for (int i = 0; i < 6 ; i++) {
+        int x;
+        cin >> x;
+        a.push_back()
+    }
+    return 0;
+}
